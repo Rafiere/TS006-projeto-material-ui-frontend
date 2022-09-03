f29858d876c13333c9952c62051feba71af101a2
Rafael <rafapgbr@gmail.com>
2022-09-03 07:38:11 -0300

    refactor! refatorando os arquivos iniciais da aplicação.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/**
+ * Quando trocamos o "function App()" pelo
+ * "export const App", ajudamos o Visual Studio Code a
+ * autocompletar o código de uma melhor maneira.
+ * 
+ */
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Testando!</p>
     </div>
   );
 }
