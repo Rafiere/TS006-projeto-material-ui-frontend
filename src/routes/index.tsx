2acffd8a97f01cda623eb16e00d0d613cf26491a
Rafael <rafapgbr@gmail.com>
2022-09-04 15:07:04 -0300

    feat: adição do Drawer para que o botão, quando clicado, abra ou feche o Drawer.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@
 
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Button } from "@mui/material";
-import { useAppThemeContext } from "../shared/contexts";
+import { useAppThemeContext, useDrawerContext } from "../shared/contexts";
 
 /**
  * Os endereços serão buscados de cima para baixo. Se a rota
@@ -16,10 +16,11 @@ import { useAppThemeContext } from "../shared/contexts";
 export const AppRoutes = () => { //Esse é um componente do React que conterá todas as rotas da aplicação.
 
     const { toggleTheme } = useAppThemeContext();
+    const { toggleDrawerOpen } = useDrawerContext();
 
     return (
         <Routes>
-            <Route path="/pagina-inicial" element={<Button variant='contained' color='primary' onClick={toggleTheme}>Toggle Theme</Button>}/>
+            <Route path="/pagina-inicial" element={<Button variant='contained' color='primary' onClick={toggleDrawerOpen}>Toggle Drawer</Button>}/>
 
             <Route path="*" element={<Navigate to="pagina-inicial"/>} />
         </Routes>
