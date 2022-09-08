af176823945ab850ea03424a4dd736b54316bc96
Rafael <rafapgbr@gmail.com>
2022-09-08 11:58:14 -0300

    feat: exportando variáveis de ambiente.

--- src/shared/environment/index.ts
+++ src/shared/environment/index.ts
@@ -3,4 +3,31 @@
  * "export" para toda a aplicação.
  */
 
-export { };
+/**
+ * Na constante "Environment", teremos todas as
+ * variáveis de ambiente que ficarão visíveis na
+ * aplicação.
+ */
+
+export const Environment = {
+
+    /**
+     * Define a quantidade de linhas a ser carregada por padrão nas listagens.
+     */
+    LIMITE_DE_LINHAS: 10,
+
+    /**
+     * Placeholder exibido nas inputs.
+     */
+    INPUT_DE_BUSCA: 'Pesquisar...',
+
+    /**
+     * Texto exibido quando nenhum registro é encontrado em uma listagem.
+     */
+    LISTAGEM_VAZIA: 'Nenhum registro encontrado.',
+
+    /**
+     * URL base de consulta dos dados da aplicação.
+     */
+    URL_BASE: 'http://localhost:3333'
+}
