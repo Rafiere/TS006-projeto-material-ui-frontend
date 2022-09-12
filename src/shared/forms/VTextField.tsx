21dd5c36021c41e23340a5f3ef52f28846c05103
Rafael <rafapgbr@gmail.com>
2022-09-11 21:11:32 -0300

    feat: corrigindo erros do botão.

--- src/shared/forms/VTextField.tsx
+++ src/shared/forms/VTextField.tsx
@@ -18,20 +18,20 @@ type TVTextFieldProps = TextFieldProps & { //Estamos dizendo que esse componente
 
 export const VTextField: React.FC<TVTextFieldProps> = ({ name, ...rest }) => { //Todas as outras propriedades do "TextFieldProps" ficarão dentro do "rest".
 
-/**
- * O "fieldName" é a propriedade que serve para identificar o componente atual, ou seja, o
- * componente "VTextField" de forma única.
- * 
- * O "registerField" servirá para pedirmos para o Unform registrar o "VTextField", que é esse componente, dentro
- * do contexto do Unform.
- * 
- * O "defaultValue" é o valor padrão de um determinado campo. Ele deverá ser passado como parâmetro para essa
- * tag.
- * 
- * O "error" é uma propriedade que permitirá a passagem de erros para esse componente.
- * 
- * O "clearError" armazena uma forma desse componente limpar o erro de forma automática.
- */
+    /**
+     * O "fieldName" é a propriedade que serve para identificar o componente atual, ou seja, o
+     * componente "VTextField" de forma única.
+     * 
+     * O "registerField" servirá para pedirmos para o Unform registrar o "VTextField", que é esse componente, dentro
+     * do contexto do Unform.
+     * 
+     * O "defaultValue" é o valor padrão de um determinado campo. Ele deverá ser passado como parâmetro para essa
+     * tag.
+     * 
+     * O "error" é uma propriedade que permitirá a passagem de erros para esse componente.
+     * 
+     * O "clearError" armazena uma forma desse componente limpar o erro de forma automática.
+     */
 
     const { fieldName, registerField, defaultValue, error, clearError } = useField(name);
 
@@ -53,10 +53,16 @@ export const VTextField: React.FC<TVTextFieldProps> = ({ name, ...rest }) => { /
             helperText={error}
             defaultValue={defaultValue}
 
-            onKeyDown={() => error ? clearError() : undefined}
+            onKeyDown={(evento) => {
+                error && clearError();
+                rest.onKeyDown?.(evento);
+            }}
 
             value={value}
-            onChange={evento => setValue(evento.target.value)}
+            onChange={evento => {
+                setValue(evento.target.value);
+                rest.onChange?.(evento);
+            }}
         />
     );
 }
