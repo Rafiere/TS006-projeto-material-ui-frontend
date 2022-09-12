f4950f7d31a6e99e0adf1525f5b1736d55b30875
Rafael <rafapgbr@gmail.com>
2022-09-12 16:24:36 -0300

    feat: exportando as telas de cidades.

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -4,5 +4,9 @@
  */
 
 export * from './dashboard/Dashboard';
+
 export * from './pessoas/ListagemDePessoas';
 export * from './pessoas/DetalheDePessoas';
+
+export * from './cidades/ListagemDeCidades';
+export * from './cidades/DetalheDeCidades';
